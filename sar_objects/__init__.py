af7a89a9327baefb093903ec5e301052e4effca8
MarkGoldman <goldmanm@mit.edu>
2017-11-28 18:21:09 -0500

    added __init__ file for loading SARs

---
+++ sar_objects/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from atkinson2007 import sar as sar_a
+from vereecken2009 import sar as sar_v
+from orlando2003 import sar as sar_o
+from mereau2000 import sar as sar_m
+
+sars = [sar_a,sar_v,sar_o,sar_m]
